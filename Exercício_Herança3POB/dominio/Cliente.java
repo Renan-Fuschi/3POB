57feab9bde3994a2be2a3f960ac3020968a30811
Renan-Fuschi <167696254+Renan-Fuschi@users.noreply.github.com>
2024-10-24 08:44:43 -0300

    Add files via upload

---
+++ Exercício_Herança3POB/dominio/Cliente.java
@@ -0,0 +1,16 @@
+package Exercicio_Herança.dominio;
+
+public abstract class Cliente {
+    protected String endereco;
+    protected String telefone;
+
+    // Construtor
+    public Cliente(String endereco, String telefone) {
+        this.endereco = endereco;
+        this.telefone = telefone;
+    }
+
+    // Método abstrato para exibir dados (será implementado nas subclasses)
+    public abstract void exibirDados();
+
+}
